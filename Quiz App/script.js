ea069c0587428c7c5e78199a12672c6007fd2a39
Brandon Ho <brandonho5@gmail.com>
2021-10-12 21:52:59 -0600

    Initial commit

---
+++ Quiz App/script.js
@@ -0,0 +1,61 @@
+const quizData = [
+    {
+        question: 'Random question 1',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        correct: 'a'
+    },
+    {
+        question: 'Random question 2',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        correct: 'b'
+    },
+    {
+        question: 'Random question 3',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        correct: 'c'
+    },
+    {
+        question: 'Random question 4',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        correct: 'd'
+    },
+    {
+        question: 'Random question 5',
+        a: 'a',
+        b: 'b',
+        c: 'c',
+        d: 'd',
+        correct: 'a'
+    },
+]
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+ 
+let currentQuestion = 0
+
+loadQuiz();
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuestion]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+    currentQuestion++
+}
